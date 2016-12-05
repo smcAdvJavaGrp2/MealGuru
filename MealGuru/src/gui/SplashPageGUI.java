4143fa95d12f6543e9892720cd5908cecf137bb5
JKeirans <JKeirans@gmail.com>
2016-12-05 09:50:09 -0800

    started guru obj, added to SplashPage, edited css

--- MealGuru/src/gui/SplashPageGUI.java
+++ MealGuru/src/gui/SplashPageGUI.java
@@ -1,6 +1,9 @@
 package gui;
 
+import java.util.Random;
+
 import data.mealguru.UserDA;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -8,23 +11,42 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import user.User;
 import utility.ResourceManager;
 
 class SplashPageGUI extends BorderPane {
-
+	Random random;
 	TextField username;
 	TextField password;
 	Button submit;
 	Button newUser;
 	Guru guru;
-	public SplashPageGUI() {
+	String[] tips = {
+			"Hi welcome to MealGuru, I am the MealGuru! I'm here to assist you!",
+			"MealGuru lets you create meals and track your nutrition.",
+			"You can eat healthy, I'm here to help you!",
+			"If you are a new user click on 'New User' to make a new account.",
+			"If you forgot your password, you're shit out of luck"};
 
+	public SplashPageGUI() {
+		random = new Random();
+		random.setSeed(System.currentTimeMillis());
+		// Create Guru and start animation
+		this.guru = new Guru(200,200);
+		this.guru.startAnimation();
+		// Supposed to return a random String from tip array
+		this.guru.enableTips(this.tips[random.nextInt(this.tips.length)]);
+
+		// Attempt to move guru to mouse click
+		this.setOnMouseClicked(e -> {
+			this.guru.move(e.getSceneX(),e.getSceneY());
+	        });        
+		
 		// GRAPHICS
-		guru = new Guru();
-		guru.getPath();
+		
 		ImageView genie = new ImageView(ResourceManager.getResourceImage("logo.png"));
 		genie.setPreserveRatio(true);
 		genie.setFitHeight(200);
@@ -63,7 +85,7 @@ class SplashPageGUI extends BorderPane {
 		this.submit.setOnAction(e -> {
 			this.submit();
 		});
-
+				
 		// CREATE A NEW USER
 
 		this.newUser = new Button("New User");
@@ -77,10 +99,10 @@ class SplashPageGUI extends BorderPane {
 		VBox center = new VBox(5, genie, this.username, this.password, this.submit, this.newUser);
 		center.setAlignment(Pos.CENTER);
 		this.setCenter(center);
+		
+		// Add guru to this borderPane and set its x, y position
 		this.getChildren().add(this.guru);
-	
-		this.guru.setLayoutX(500);
-		this.guru.setLayoutY(500);
+
 
 	}
 
@@ -110,9 +132,6 @@ class SplashPageGUI extends BorderPane {
 				PrimaryWindow.displayMainGUI();
 
 			}
-
 		}
-		
-
 	}
 }
